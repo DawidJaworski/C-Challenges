2259cd489d3634d355aa3d6f1c3fb1378144f518
DawidJaworski <jaworski.dawid@protonmail.com>
2020-10-31 21:35:38 +0100

    Finished parsing method

--- [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
+++ [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
@@ -12,44 +12,113 @@ std::string parseBrackets(std::string);
 void printDictionary(dictionary);
 void printTest(uint, test);
 bool testMethod(test);
+int checkInput(std::string);
 
 int main(){
   printf("[4/24/2014] Challenge #154 [Easy] March Madness Brackets\n");
 
-  dictionary dict;
-  dictionary results;
+  dictionary dict, results;
   dict.insert(std::pair<uint, test>(0, test(
     "((your[drink {remember to}]) ovaltine)",
     "remember to drink your ovaltine")));
   dict.insert(std::pair<uint, test>(1, test(
     "[racket for {brackets (matching) is a} computers]",
-    "matching brackets is a  racket for computers")));
+    "matching brackets is a racket for computers")));
   dict.insert(std::pair<uint, test>(2, test(
     "[can {and it(it (mix) up ) } look silly]",
     "mix it up and it can look silly")));
   dict.insert(std::pair<uint, test>(3, test(
     "{years [four score] ago (and seven) our fathers}",
     "four score and seven years ago our fathers")));
+  results = dictionary(dict);
 
-  std::cout << "Pattern:" << std::endl;
+  std::cout << "\nPattern:" << std::endl;
   printDictionary(dict);
 
-  std::cout << "Results:" << std::endl;
+  for (dictionary::iterator it = results.begin(); it != results.end(); ++it){
+    it->second.second = parseBrackets((it->second).first);
+  }
+
+  std::cout << "\nResults:" << std::endl;
   printDictionary(results);
 
   return 0;
 }
 
 std::string parseBrackets(std::string input){
-  std::string output;
-  output = "bullshitsssdd";
+  std::string output,buffer;
+  std::string inputBuffer = input;
+  std::map<char,char> brackets = {
+    {'(',')'},
+    {'[',']'},
+    {'{','}'}
+  };
+  std::map<char,char>::iterator it;
+  size_t openingBracket = input.length();
+  size_t closingBracket = input.length();
+  size_t matchPos = 0;
+
+  uint counter = 0;
+  while(true){
+    // find last opening bracket from left
+    matchPos = inputBuffer.find_last_of({'(','[','{'}, openingBracket);
+    if(matchPos == std::string::npos){
+      break;
+    }
+    openingBracket = matchPos;
+  
+    // get matching closing bracket
+    it = brackets.find(input[matchPos]);
+    if(it == brackets.end())
+      continue;
+
+    // search for nearest matching closing bracket to right
+    matchPos = inputBuffer.find_first_of(it->second, openingBracket);
+    if(matchPos == std::string::npos){
+      break;
+    }
+    closingBracket = matchPos;
+    
+    // extract matched brackets and remove it from input buffer
+    buffer = inputBuffer.substr(openingBracket, closingBracket+1-openingBracket);
+    inputBuffer = inputBuffer.erase(openingBracket, closingBracket+1-openingBracket);
+
+    // dispose enclosing brackets
+    buffer = buffer.substr(1,buffer.length()-2);
+
+    // remove space if present at the start of the string
+    if(buffer.at(0) == ' ')
+      buffer.erase(0, 1);
+
+    // add space if not present at the end of string
+    if(buffer.at(buffer.length()-1) != ' ')
+      buffer.append(" ");
+
+    // remove double spaces from strings inside
+    matchPos = buffer.find("  ");
+    if(matchPos != std::string::npos)
+      buffer.erase(matchPos, 1); 
+    
+    // appent buffer to output
+    output.append(buffer);
+    
+    counter++;
+    if(counter >= 4)
+      break;
+  }
+
   return output;
 }
 
 bool testMethod(test pattern){
-  bool result = false;
+  return (bool) pattern.second.compare(parseBrackets(pattern.first));
+}
 
-  return result;
+int checkInput(std::string input){
+  bool result = false;
+  
+
+  return 0;
 }
 
 void printDictionary(dictionary dict){
@@ -58,8 +127,5 @@ void printDictionary(dictionary dict){
 }
 
 void printTest(uint index, test input){
-  printf("[%d]\t", index);
-  // printf() has problems with std::string. I have no negotiating skills for that
-  std::cout << input.first << "\t->\t";
-  std::cout << input.second << "\n";
+  printf("[%2d]\t%50s\t->\t%-50s\n", index, input.first.data(), input.second.data());
 }
