e36e0342c98f128f56e2fb8f708931cb5b048d3e
DawidJaworski <jaworski.dawid@protonmail.com>
2020-11-02 02:21:21 +0100

    Done bracket matching method

--- [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
+++ [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
@@ -4,17 +4,17 @@
 #include <string>
 #include <map>
 
-
-
-typedef unsigned int uint;
 typedef std::map<std::string, std::string> dictionary;
-typedef std::map<char,char> symbols;
+
 typedef std::map<size_t, size_t> coords;
+typedef std::map<char,char> symbols;
 typedef std::map<size_t, char> data;
 typedef std::pair<size_t, char> record;
 
-data debugMethod(std::string);
-data debugMethod(std::string, bool);
+data findBrackets(std::string);
+data findBrackets(std::string, bool);
+coords checkErrors(data);
+char getMatchingBracket(char);
 void printDictionary(dictionary);
 
 std::string _ERROR;
@@ -25,41 +25,68 @@ dictionary dict = {
   {"{years [four score] ago (and seven) our fathers}","four score and seven years ago our fathers"}
 };
 char brackets [6] = {'(','[','{',')',']','}'};
+//symbols bracketMap = {{'(',')'},{')','('},{'[',']'},{']','['},{'{','}'},{'}','{'}};
+symbols bracketMap = {{'(',')'},{'[',']'},{'{','}'}};
 
 int main(){
-  printf("[4/24/2014] Challenge #154 [Easy] March Madness Brackets\n");
+  printf("[4/24/2014] Challenge #154 [Easy] March Madness Brackets\n\n");
   data output;
 
-  output = debugMethod(dict.begin()->first, true);
+  output = findBrackets(dict.begin()->first, true);
+
+
+
   printf("Error:\t%s\n", _ERROR.data());
 
   return 0;
 }
 
-data debugMethod(std::string input){
-  return debugMethod(input, false);
+data findBrackets(std::string input){
+  return findBrackets(input, false);
 }
-
-data debugMethod(std::string input, bool verbose){
+data findBrackets(std::string input, bool verbose){
   data results;
-  size_t matchPos = 0;
+  size_t match = 0;
 
-  while(matchPos = input.find_first_of(brackets, matchPos) != std::string::npos) {
-    printf("%d\n", matchPos);
-    results.insert(record(matchPos, input[matchPos++]));
+  match = input.find_first_of(brackets);
+  while(match != std::string::npos) {
+    results.insert(record(match, input[match]));
+    match = input.find_first_of(brackets, ++match);
   }
   
   if(verbose){
-    printf("[3%d]\t%s\n", input.length()-1, input.data());
+    printf("[%3d]\t%s\n", input.length()-1, input.data());
     for (data::iterator it = results.begin(); it != results.end(); ++it) 
-      printf("[%d]\t\'%c\'\n", it->first, it->second);
+      printf("> [%3d]\t\'%c\'\n", it->first, it->second);
   }
 
   return results;
 }
 
+coords checkErrors(data input){
+  coords matches;
+  data inputCopy = data(input);
+  while(inputCopy.size() > 0){
+    for (data::iterator it = inputCopy.begin(); it != inputCopy.end(); ++it) {
+      if(++it->second == getMatchingBracket(it->second)){
+        matches.insert(record(it->first, (++it)->first));
+        inputCopy.erase(it, ++it);
+        break;
+      }
+      
+      
+    }
+  }
+  
+  return matches;
+}
+
+char getMatchingBracket(char bracket){
+    return bracketMap.find(bracket)->second;
+}
+
 void printDictionary(dictionary input){
-  uint maxLength = 0;
+  size_t maxLength = 0;
   for (dictionary::iterator it = input.begin(); it != input.end(); ++it){
     if(it->first.length() > maxLength)
       maxLength = it->first.length();
