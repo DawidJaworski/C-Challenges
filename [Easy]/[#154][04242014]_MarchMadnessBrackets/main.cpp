6ff83d0e043272e8a376b7c683188b0ca3d9d73f
DawidJaworski <jaworski.dawid@protonmail.com>
2020-11-03 16:15:32 +0100

    Changed matching bracket output to queue from map

--- [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
+++ [Easy]/[#154][04242014]_MarchMadnessBrackets/main.cpp
@@ -9,12 +9,14 @@ typedef std::map<std::string, std::string> dictionary;
 typedef std::map<size_t, size_t> coords;
 typedef std::map<char,char> symbols;
 typedef std::map<size_t, char> data;
+typedef std::pair<size_t, size_t> coord;
 typedef std::pair<size_t, char> record;
 
 data findBrackets(std::string);
 data findBrackets(std::string, bool);
 coords matchBrackets(data);
 char getMatchingBracket(char);
+bool isClosingBracket(char);
 void printDictionary(dictionary);
 
 std::string _ERROR;
@@ -27,8 +29,8 @@ const dictionary dict = {
   {"[can {and it(it (mix) up ) look silly]", "Missing closing bracket"},
   {"[racket for brackets (matching) is a} computers]", "Missing opening bracket"}
 };
-const char brackets [6] = {'(','[','{',')',']','}'};
-const symbols bracketMap = {{'(',')'},{'[',']'},{'{','}'}};
+static const char brackets [6] = {'(','[','{',')',']','}'};
+static const symbols bracketMap = {{'(',')'},{'[',']'},{'{','}'}};
 
 int main(){
   printf("[4/24/2014] Challenge #154 [Easy] March Madness Brackets\n\n");
@@ -37,10 +39,12 @@ int main(){
 
   for (auto input = dict.begin(); input != dict.end(); ++input)
   {
+    std::cout << "=======================================" << std::endl;
     foundBrackets = findBrackets(input->first, true);
     foundMatchingBrackets = matchBrackets(foundBrackets);
 
     printf("Error:\t%s\n", _ERROR.data());
+    //break;
   }
 
   return 0;
@@ -70,36 +74,57 @@ data findBrackets(std::string input, bool verbose){
 
 coords matchBrackets(data input){
   coords results;
-  bool foundFirstClosingBracket = false;
+  bool exitLoop = false;
   record lastOperningBracket = record(-1, '#');
   record firstClosingBracket = record(-1, '#');
+  _ERROR = "";
+
 
   data inputCopy = data(input);
+  printf("[%6s]\tinputCopy size = %d\n", "Before", inputCopy.size());
   while(inputCopy.size() > 0){
-    for (data::iterator it = inputCopy.begin(); it != inputCopy.end(); ++it) {
-      if(++it->second == getMatchingBracket(it->second)){
-        results.insert(record(it->first, (++it)->first));
-        inputCopy.erase(it, ++it);
+    for (data::iterator it = inputCopy.begin(), itt = ++(inputCopy.begin()); it != --(inputCopy.end()); ++it, ++itt) {
+      char matchingBracket = getMatchingBracket(it->second);
+      char nextBracket = itt->second;
+      printf("Checking: %2d: '%c'->'%c' == %2d: '%c'\n", it->first, it->second, matchingBracket, itt->first, itt->second);
+      if(itt->second == matchingBracket){
+        printf("Found match: %2d '%c' & %2d '%c'\n", it->first, it->second, itt->first, itt->second);
+        results.insert(coord(it->first, itt->first));
+        inputCopy.erase(itt);
+        inputCopy.erase(it);
+        break;
+      }else if(isClosingBracket(itt->second)){
+        char err [50];
+        sprintf(err, "Mismatched bracket %c instead of %c found at %d", itt->second, matchingBracket, itt->first);
+        _ERROR = std::string(err);
+        exitLoop = true;
         break;
       }
-    }  
-
-    break;
-  }
-  
-  for (data::iterator it = inputCopy.begin(); it != inputCopy.end(); ++it){
-    if(bracketMap.find(it->second) != bracketMap.end())
-      lastOperningBracket = record(*it);
+    }
+    if(exitLoop) break;
   }
 
-  printf("%-22s\t%3d %c\n", "Last opening bracket:", lastOperningBracket.first, lastOperningBracket.second);
-  printf("%-22s\t%3d %c\n", "First closing bracket:", firstClosingBracket.first, firstClosingBracket.second);
+  printf("results size = %d\n", results.size());
+  if(results.size() > 0)
+    for (auto it = results.begin(); it != results.end(); ++it)
+      printf("(%3d;%3d)\n", it->first, it->second);
 
   return results;
 }
 
 char getMatchingBracket(char bracket){
-    return bracketMap.find(bracket)->second;
+    char result = '#';
+    symbols::const_iterator it = bracketMap.find(bracket);
+    if(it != bracketMap.end())
+      result = it->second;
+    return result;
+}
+
+bool isClosingBracket(char bracket){
+  for(size_t index = 3; index < 6; index++)
+    if(bracket == brackets[index])
+      return true;
+  return false;
 }
 
 void printDictionary(dictionary input){
