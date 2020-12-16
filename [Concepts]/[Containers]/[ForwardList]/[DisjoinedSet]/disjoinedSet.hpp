2c58849d4eb8e47d92a14204dfeae172039f5301
DawidJaworski <jaworski.dawid@protonmail.com>
2020-12-16 19:24:13 +0100

    Added Find() definition

--- [Concepts]/[Containers]/[ForwardList]/[DisjoinedSet]/disjoinedSet.hpp
+++ [Concepts]/[Containers]/[ForwardList]/[DisjoinedSet]/disjoinedSet.hpp
@@ -12,7 +12,9 @@ class disjoinedSet{
   public:
     void makeSet(T element);
     void mergeSets(std::forward_list<T> parent, std::forward_list<T> child);
-    typename std::forward_list<T>::iterator find(T element);
+    typename std::vector<std::forward_list<T>>::iterator find(T element);
+    typename std::vector<std::forward_list<T>>::iterator Begin();
+    typename std::vector<std::forward_list<T>>::iterator End();
     int getCount();
 };
 
@@ -28,10 +30,19 @@ void disjoinedSet<T>::mergeSets(std::forward_list<T> parent, std::forward_list<T
 }
 
 template <typename T>
-typename std::forward_list<T>::iterator disjoinedSet<T>::find(T element){
-  auto result = std::find_if(std::begin(data), std::end(data), 
+typename std::vector<std::forward_list<T>>::iterator disjoinedSet<T>::find(T element){
+  return std::find_if(std::begin(data), std::end(data), 
     [element](auto x) { return (std::find(std::begin(x), std::end(x), element) != std::end(x)); });
-  return result;
+}
+
+template <typename T>
+typename std::vector<std::forward_list<T>>::iterator disjoinedSet<T>::Begin(){
+  return std::begin(data);
+}
+
+template <typename T>
+typename std::vector<std::forward_list<T>>::iterator disjoinedSet<T>::End(){
+  return std::end(data);
 }
 
 template <typename T>
--- [Concepts]/[Containers]/[ForwardList]/[DisjoinedSet]/main.cpp
+++ [Concepts]/[Containers]/[ForwardList]/[DisjoinedSet]/main.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
+#include <typeinfo>
 
 #include "disjoinedSet.hpp"
 
 int main(){
-
-  std::cout << "Disjoined Set:" << std::endl;
+  std::cout << "Disjoined Set example:" << std::endl;
 
   disjoinedSet<int> ds;
   ds.makeSet(1);
@@ -15,7 +15,14 @@ int main(){
 
   std::cout << "No. sets: " << ds.getCount() << std::endl;
 
-  ds.find(3);
+  int target[2] {3,100};
+  std::for_each(std::begin(target), std::end(target), [&ds, target](auto x){
+    std::cout << "Find target set containing: " << x << std::endl;
+    std::cout << (ds.find(x) != ds.End() ? "Target found" : "Target not found") << std::endl;
+  });
+
+  
+
   return 0;
 }
 
